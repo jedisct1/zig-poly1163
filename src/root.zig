9d6d8eed263a7d372895ab01c8e501a058428725
Frank Denis <github@pureftpd.org>
2025-08-12 19:19:43 +0200

    Remove useless comments

--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,10 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-// Import the optimized vector implementation
 const vector = @import("vector.zig");
 
-// Export the optimized implementation
 pub const Poly1163 = vector.Poly1163;
 pub const authenticate = vector.authenticate;
 
-// Constants
 pub const TAG_SIZE = 16;
 pub const KEY_SIZE = 32;
